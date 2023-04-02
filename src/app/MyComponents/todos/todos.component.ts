23d0bbdd9b13dd4844b768298fbf82371946e2bd
Payal Adhikary <payaladhikary2000@gmail.com>
2023-04-02 10:12:58 +0530

    first commit

---
+++ src/app/MyComponents/todos/todos.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Todo } from 'src/app/Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent {
+  localItem!: string|null;
+  todos!: Todo[];
+  constructor(){
+    this.localItem = localStorage.getItem("todos");
+    if(this.localItem == null){
+      this.todos = []
+    }else{
+      this.todos = JSON.parse(this.localItem);
+    }
+  }
+
+  addTodo(todo:Todo){
+    this.todos.push(todo);
+    localStorage.setItem("todos",JSON.stringify(this.todos));
+  }
+  onClick(todo: Todo){
+    const index = this.todos.indexOf(todo);
+    this.todos.splice(index,1);
+    localStorage.setItem("todos",JSON.stringify(this.todos));
+  }
+  done(todo: Todo){
+    const index = this.todos.indexOf(todo);
+    this.todos[index].active = !this.todos[index].active;
+    localStorage.setItem("todos",JSON.stringify(this.todos));
+  }
+}
